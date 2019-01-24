df34514b8d0ca478e87c38ba2a5a038c873c79dc
Rasmus Wiuff <rwiuff@gmail.com>
2019-01-24 18:09:57 +0100

    Merge Commit

--- Listings/CountRateIU.m
+++ Listings/CountRateIU.m
@@ -2,21 +2,28 @@ close all
 clear all
 
 foldername = '../Data';
-Hydrogenfile = 'Hydrogen.txt';
+Hydrogenfile = 'Contours.txt';
 fullfilename = fullfile(foldername, Hydrogenfile);
-H = dlmread(fullfilename);
+C = dlmread(fullfilename);
 
-Deuteriumfile = 'Deuterium.txt';
-fullfilename = fullfile(foldername, Deuteriumfile);
-D = dlmread(fullfilename);
+I = C(:,1);
+V = C(:,2);
+P = C(:,3);
+Cts = C(:,4);
+Flux = C(:,5);
+ns = C(:,6);
 
+x = I;
+y = V;
+z = Cts;
+[X,Y]=meshgrid(x,y);
+Z = reshape(z,numel(y),numel(x));
+contourf(X,Y,Z)
 
-
-
-mkdir('../MatlabFigures', 'Asign3')
-foldername = '../MatlabFigures/Asign3';
-epsfilename = 'PlasmaUI.eps';
-fullfilename = fullfile(foldername, epsfilename);
-saveas(q1, fullfilename, 'epsc')
-str = 'Plot saved';
-disp(str);
+% mkdir('../MatlabFigures', 'Asign3')
+% foldername = '../MatlabFigures/Asign3';
+% epsfilename = 'PlasmaUI.eps';
+% fullfilename = fullfile(foldername, epsfilename);
+% saveas(q1, fullfilename, 'epsc')
+% str = 'Plot saved';
+% disp(str);
